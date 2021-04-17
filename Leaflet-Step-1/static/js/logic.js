28d0c50bb8f3a6f89ae85fc11df5f8ca98cdcd5e
carterkioski <carterkioski@gmail.com>
2021-04-17 09:42:41 -0400

    rename folders, add url for earthquakes

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,15 @@
+url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
+
+
+/*
+{"type":"FeatureCollection",
+"metadata":{"generated":1618629888000,"url":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson","title":"USGS Magnitude 4.5+ Earthquakes, Past Month","status":200,"api":"1.10.3","count":488},
+"features":[{"type":"Feature","properties":{"mag":5.3,"place":"Mariana Islands region","time":1618622841617,
+    "updated":1618626451919,"tz":null,"url":"https://earthquake.usgs.gov/earthquakes/eventpage/us6000e29p",
+    "detail":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us6000e29p.geojson",
+    "felt":null,"cdi":null,"mmi":null,"alert":null,"status":"reviewed","tsunami":0,"sig":432,"net":"us","code":"6000e29p",
+    "ids":",us6000e29p,","sources":",us,","types":",oaf,origin,phase-data,","nst":null,"dmin":6.254,"rms":0.82,"gap":58,
+    "magType":"mb","type":"earthquake","title":"M 5.3 - Mariana Islands region"},
+    "geometry":{"type":"Point","coordinates":[145.9753,21.511,10]},"id":"us6000e29p"},
+
+*/
