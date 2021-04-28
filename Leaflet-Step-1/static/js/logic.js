d3bfcb76d0828a60ada49d5355a6ee268e2ab19e
carterkioski <carterkioski@gmail.com>
2021-04-27 22:43:24 -0400

    readme update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -12,7 +12,6 @@ function createMap(earthquakeMarkers) {
       "Light Map": lightmap
     };
   
-    // Create an overlayMaps object to hold the bikeStations layer
     var overlayMaps = {
       "Earthquakes": earthquakeMarkers
     };
@@ -36,14 +35,10 @@ function createMarkers(response) {
     var earthquakes = response.features
     var earthquakeMarkers = [];
   
-    // Loop through the stations array
     for (var index = 0; index < earthquakes.length; index++) {
       var location = earthquakes[index].geometry.coordinates
       var magnitude = earthquakes[index].properties.mag
-      // For each station, create a marker and bind a popup with the station's name
-      //var earthquakeMarker = L.marker([location[0], location[1]])
-        
-      
+    
       var earthquakeMarker = L.circle([location[0], location[1]], {
         color: 'red',
         fillColor: '#f03',
@@ -53,68 +48,9 @@ function createMarkers(response) {
                     "<h3>Longitude: "+ location[1] + "</h3>"+
                     "<h3>Depth: "+ location[2] + "</h3>"+
                     "<h3>Magnitude: " + magnitude + "</h3>");
-      // Add the marker to the bikeMarkers array
       earthquakeMarkers.push(earthquakeMarker);
     }
-  
-    // Create a layer group made from the bike markers array, pass it into the createMap function
-    createMap(L.layerGroup(earthquakeMarkers));
+      createMap(L.layerGroup(earthquakeMarkers));
   }
 
-
-
-
-url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson'
-
 d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson').then(createMarkers);
-
-/*
-{
-    "type":"FeatureCollection",
-    "metadata":{
-        "generated":1618629888000,
-        "url":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson",
-        "title":"USGS Magnitude 4.5+ Earthquakes, Past Month",
-        "status":200,
-        "api":"1.10.3",
-        "count":488
-    },
-    "features":[
-        {
-            "type":"Feature",
-            "properties":{
-                "mag":5.3,
-                "place":"Mariana Islands region",
-                "time":1618622841617,
-                "updated":1618626451919,
-                "tz":null,
-                "url":"https://earthquake.usgs.gov/earthquakes/eventpage/us6000e29p",
-                "detail":"https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/us6000e29p.geojson",
-                "felt":null,
-                "cdi":null,
-                "mmi":null,
-                "alert":null,
-                "status":"reviewed",
-                "tsunami":0,
-                "sig":432,
-                "net":"us",
-                "code":"6000e29p",
-                "ids":",us6000e29p,",
-                "sources":",us,",
-                "types":",oaf,origin,phase-data,",
-                "nst":null,
-                "dmin":6.254,
-                "rms":0.82,
-                "gap":58,
-                "magType":"mb",
-                "type":"earthquake",
-                "title":"M 5.3 - Mariana Islands region"
-            },
-            "geometry":{
-            "type":"Point",
-            "coordinates":[145.9753,21.511,10]
-        },
-    "id":"us6000e29p"
-},
-
-*/
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # leaflet-challenge
+This was completed by Carter Kioski
+
+In this assignment, I am hitting an API to get information about earthquakes from around the world.  With this info, I am utilizing leaflet.js to plot the earthquakes on the map with circles that change shape based on the magnitude of each earthquake.  Clicking on the earthquake circle provides more detail about the earthquake. 
